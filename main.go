5f5fd8b924faa1ce025f57a03cfc2e3d0a2419d5
lvyxxx <34331679+lvyxxx@users.noreply.github.com>
2021-05-25 16:54:16 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import(
-  "fmt"
+import (
+	"log"
+	"net/http"
 )
 
-func main(){
-  fmt.Println("123")
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("Hello World!"))
+}
+func main() {
+
+	//1.注册一个给定模式的处理器函数到DefaultServeMux
+	http.HandleFunc("/", sayHello)
+
+	//2.设置监听的TCP地址并启动服务
+	//参数1：TCP地址(IP+Port)
+	//参数2：当设置为nil时表示使用DefaultServeMux
+	err := http.ListenAndServe(":9999", nil)
+	log.Fatal(err)
 }
